ad1a0a642b672d218d751de6234355d58aad3b5f
PEILLON Etienne <etienne.peillon@ensta-paristech.fr>
2019-06-05 16:46:43 -0700

    aa

--- Matlab/view_real.m
+++ Matlab/view_real.m
@@ -17,7 +17,7 @@ if (kc==kd)
     X = linspace(c,d,pt);
 else
     X = zeros((kd-kc+1)*pt,1);
-    X(1:pt) = linspace(Edge(E2edge(kc,1)),Edge(E2edge(kd,2)),pt);
+    X(1:pt) = linspace(c,Edge(E2edge(kc,2)),pt);
     for i=2:kd-kc
         X((i-1)*pt+1:i*pt) = linspace(Edge(E2edge(kc+i-1,1)),Edge(E2edge(kc+i-1,2)),pt);
     end
